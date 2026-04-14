fe00c06c0649caf568767c9168bb672a70ed28e6
sacuLM <polino079@gmail.com>
2026-04-14 20:20:02 -0300

    feat: schema pacientes completo

--- src/db/schema/pacientes.ts
+++ src/db/schema/pacientes.ts
@@ -1,13 +1,13 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, char,text } from "drizzle-orm/pg-core";
 
 export const pacientes = pgTable("pacientes", {
     nome: varchar("nome_paciente", { length: 255 }).notNull(),
     nascimento: varchar("data_nascimento_paciente", { length: 8 }).notNull(),
     id_paciente: varchar("id_paciente", { length: 36 }).primaryKey(),
-    endereco
-    telefone
-    convenio
-    hf
-    hs
+    endereco: varchar("endereco_paciente", { length: 255 }).notNull(),
+    telefone: char("telefone_paciente", { length: 11 }).notNull(),
+    convenio: varchar("convenio_medico_paciente", { length: 40 }).notNull(),
+    hf: text("hf_paciente"),
+    hs: text("hs_paciente"),
 
   });
